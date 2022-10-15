b48d1e17512f8fab28baf10e1c27de637193f07a
simon <simonakuliesho@gmail.com>
2022-10-15 17:10:17 +0300

    Add  wait , clickElement and getElement methods to Common class

--- src/test/java/piguLt/pages/Common.java
+++ src/test/java/piguLt/pages/Common.java
@@ -1,6 +1,13 @@
 package piguLt.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import piguLt.utils.Driver;
+
+import java.time.Duration;
 
 public class Common {
 
@@ -12,5 +19,18 @@ public class Common {
 
     public static void openUrl(String url) {
         Driver.getDriver().get(url);
+    }
+
+    public static void waitForElementChangeVisability(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
     }
 }
