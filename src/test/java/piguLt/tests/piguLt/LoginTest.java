ab213f7e5f243a9644a44713873665e2f8c8a53d
simon <simonakuliesho@gmail.com>
2022-10-15 16:26:36 +0300

    Implement LoginTest class extends TestBaseclass

--- src/test/java/piguLt/tests/piguLt/LoginTest.java
+++ src/test/java/piguLt/tests/piguLt/LoginTest.java
@@ -1,7 +1,12 @@
 package piguLt.tests.piguLt;
 
+import org.testng.annotations.BeforeMethod;
 import piguLt.tests.TestBase;
 
 public class LoginTest extends TestBase {
-
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+    }
 }
