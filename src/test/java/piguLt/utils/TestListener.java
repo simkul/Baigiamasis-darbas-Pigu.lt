8f33f4b394c34af7e2e01137eb096fc1c56d5bcd
simon <simonakuliesho@gmail.com>
2022-10-15 21:56:58 +0300

    Implement TestListener method

--- src/test/java/piguLt/utils/TestListener.java
+++ src/test/java/piguLt/utils/TestListener.java
@@ -1,7 +1,14 @@
 package piguLt.utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class TestListener implements ITestListener {
     @Override
@@ -9,5 +16,18 @@ public class TestListener implements ITestListener {
         takeScreenshot();}
 
     private void takeScreenshot() {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        File screenshotRaw =  takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String directory = "./screenshots";
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd_HH_mm_ss");
+        String  dateTime = localDateTime.format(formatter);
+
+        File screenshotFile = new File(directory +
+                "/screenshot" + dateTime +"_"+ UUID.randomUUID()+ ".png");
+
+
     }
 }
