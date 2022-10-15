32e72018ae3c94ecedc7184e6315b83df96fa51d
simon <simonakuliesho@gmail.com>
2022-10-15 20:34:44 +0300

    Add methods to CartPage

--- src/test/java/piguLt/pages/piguLt/CartPage.java
+++ src/test/java/piguLt/pages/piguLt/CartPage.java
@@ -1,9 +1,42 @@
 package piguLt.pages.piguLt;
 
 import piguLt.pages.Common;
+import piguLt.pages.Locators;
 
 public class CartPage {
     public static void open(String url) {
         Common.openUrl(url);
+    }
+
+    public static void clickDouble() {
+        Common.doubleClickByActions(Locators.PiguLt.Cart.section);
+    }
+
+    public static void enterNeededAmount(String neededAmount) {
+        Common.sendKeysToElement(neededAmount,
+                Locators.PiguLt.Cart.section);
+    }
+    public static void clickOnAddButto() {
+        Common.clickElement(Locators.PiguLt.Cart.AddToCartButton);
+    }
+
+    public static void waitForCartForm() {
+        Common.waitForElementChangeVisability(Locators.PiguLt.Cart.cartForm);
+    }
+
+    public static void closeCartForm() {
+        Common.clickElement(Locators.PiguLt.Cart.closeButton);
+    }
+
+    public static void clickOnCartIcon() {
+        Common.clickElement(Locators.PiguLt.Cart.cartIcon);
+    }
+
+    public static void waitForCarPageLoad() {
+        Common.waitForElementChangeVisability(Locators.PiguLt.Cart.cartPage);
+    }
+
+    public static String readItemName() {
+        return Common.getElementText(Locators.PiguLt.Cart.itemName);
     }
 }
