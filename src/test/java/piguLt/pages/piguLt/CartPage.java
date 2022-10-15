fda99ce12a254642cdf0ef7770c229b07477f94a
simon <simonakuliesho@gmail.com>
2022-10-15 19:54:11 +0300

    Add open method

--- src/test/java/piguLt/pages/piguLt/CartPage.java
+++ src/test/java/piguLt/pages/piguLt/CartPage.java
@@ -1,4 +1,9 @@
 package piguLt.pages.piguLt;
 
+import piguLt.pages.Common;
+
 public class CartPage {
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
 }
--- src/test/java/piguLt/tests/piguLt/CartTest.java
+++ src/test/java/piguLt/tests/piguLt/CartTest.java
@@ -1,4 +1,21 @@
 package piguLt.tests.piguLt;
 
-public class CartTest {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import piguLt.pages.piguLt.CartPage;
+import piguLt.tests.TestBase;
+
+public class CartTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        CartPage.open("https://pigu.lt/lt/kvepalai-pigiau/kvepalai-moterims/tualetinis-vanduo-dolce-gabbana-3-limperatrice-edt?id=538670");
+    }
+
+    @Test
+    private void testAddToCartItem(){
+
+
+    }
 }
