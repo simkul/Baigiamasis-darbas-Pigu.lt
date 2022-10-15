2164fb4da73a442415b5ceeb6acaee69e6464010
simon <simonakuliesho@gmail.com>
2022-10-15 20:32:42 +0300

    Add Locators to Cart class

--- src/test/java/piguLt/pages/Locators.java
+++ src/test/java/piguLt/pages/Locators.java
@@ -26,6 +26,16 @@ public class Locators {
             public static By searchedItem = By.xpath("//*[@id=\"productBlock49514933\"]/div/div/div[4]");
             public static By loadednNewPage = By.xpath("//*[@id=\"galleryThumbnails44424153\"]/div[2]/div/div[1]/div/img");
             public static By searchedItemName = By.xpath("//*[@id=\"productPage\"]/section[1]/div[1]/h1");
+        }
+        public static class Cart{
+            public static By section = By.xpath("//*[@id=\"productPage\"]/section[1]/div[1]/div[2]/div[3]/div[5]/div[2]/div/input");
+            public static By AddToCartButton = By.xpath("//*[@id=\"productPage\"]/section[1]/div[1]/div[2]/div[3]/div[5]/div[2]/button");
+            public static By cartForm = By.xpath("//*[@id=\"modal\"]/div/div/div/div[2]");
+            public static By closeButton = By.xpath("//*[@id=\"close\"]");
+            public static By cartIcon = By.xpath("//*[@id=\"cartWidget\"]/a/i");
+            public static By cartPage = By.xpath("//*[@id=\"sliceBlock124274801\"]");
+            public static By itemName = By.xpath("//*[@id=\"productSliceBlock363493696\"]/td[2]/div[1]");
+
 
 
 
