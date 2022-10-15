2f1395193168350af5ea86cda0c8f3bb2d1f39d5
simon <simonakuliesho@gmail.com>
2022-10-15 17:09:11 +0300

    Add  locators to PiguLt/Cookies

--- src/test/java/piguLt/pages/Locators.java
+++ src/test/java/piguLt/pages/Locators.java
@@ -1,4 +1,17 @@
 package piguLt.pages;
 
+import org.openqa.selenium.By;
+
 public class Locators {
+
+    public static class PiguLt{
+        public static class Cookies{
+            public static By coockieBlog = By.xpath("//*[@id='cookie_block']");
+            public static By coockieAceptButton = By.xpath("//*[@id=\"cookie_block\"]/div/div/div[2]/button");
+        }
+        public static class Login{
+
+
+        }
+    }
 }
