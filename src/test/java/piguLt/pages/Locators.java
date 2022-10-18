72d1f5a8c42714fab826a08d8e28396c66cf2042
simon <simonakuliesho@gmail.com>
2022-10-18 13:29:15 +0300

    Rewrite xpath in Login/PuguLt/Locators

--- src/test/java/piguLt/pages/Locators.java
+++ src/test/java/piguLt/pages/Locators.java
@@ -10,13 +10,18 @@ public class Locators {
             public static By coockieAceptButton = By.xpath("//button[@class='button agree']");
         }
         public static class Login{
-            public static By loginForm = By.xpath("//*[@id=\"loginModal\"]/div[1]/div[1]/form");
-            public static By inputEmail = By.xpath("//*[@id=\"loginModal\"]/div[1]/div[1]/form/div[2]/input");
-            public static  By inputPassword = By.xpath("//*[@id=\"passwordCont\"]/input");
-            public static By prisijungtiButton = By.xpath("//*[@id=\"loginModal\"]/div[1]/div[1]/form/div[5]/input");
-            public static  By profilePage = By.xpath("/html/body/div[10]/div/section[2]/div/h1");
-            public static By paragraphName = By.xpath("<div class=\"error-message\">Neteisingi prisijungimo duomenys</div>");
-            public static By errorMessage = By.xpath ("<div class=\"error-message\">Neteisingi prisijungimo duomenys</div>");
+            public static By inputEmail = By.xpath("//input[@placeholder='El. paštas'][1]");
+            public static  By inputPassword = By.xpath("//*[@id='passwordCont']/input");
+            public static By prisijungtiButton = By.xpath("//*[@value='Prisijungti']");
+            public static By slapukaiPage = By.xpath("//h1[text()='Slapukų politika']");
+            public  static  By clickSubmitButton = By.xpath("//input[@name='allCookies']");
+            public  static  By profileIcon = By.xpath("//*[@id='headeMenu']/li[1]/a/i");
+            public static  By profilePage = By.xpath("//h1[text()='Mano profilis']");
+
+
+
+
+            public static By errorMessage = By.xpath ("//div[@class='error-message']");
 
         }
         public static class Search{
